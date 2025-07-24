174deba1492a67d14e3ef27e348fa339532b4cf5
Ezzz-Lui <ogn.lui@gmail.com>
2025-07-24 05:51:12 -0600

    Add ShopLayout.vue for header and footer navigation structure

---
+++ src/modules/shop/layouts/ShopLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- Header Navbar -->
+  <HeaderNavbar />
+
+  <!-- Main Content -->
+  <router-view />
+
+  <!-- Footer -->
+  <FooterNavbar />
+</template>
+
+<script setup lang="ts">
+import FooterNavbar from '../components/FooterNavbar.vue';
+import HeaderNavbar from '../components/HeaderNavbar.vue';
+</script>
