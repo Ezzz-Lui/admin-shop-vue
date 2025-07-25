11848546b110bc4c53ed982131e05d956c1bb66f
Ezzz-Lui <ogn.lui@gmail.com>
2025-07-25 17:42:27 -0600

    Add AuthLayout component for authentication layout structure

---
+++ src/modules/auth/layouts/AuthLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <!-- Left: Image -->
+    <div class="w-1/2 h-screen hidden lg:block">
+      <img
+        src="https://placehold.co/800x/0c0a09/ffffff.png?text=Your+Image&font=Poppins"
+        alt="Placeholder Image"
+        class="object-cover w-full h-full"
+      />
+    </div>
+    <!-- Right: Form -->
+    <RouterView />
+  </div>
+</template>
