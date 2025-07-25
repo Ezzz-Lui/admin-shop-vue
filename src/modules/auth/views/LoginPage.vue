e3e2c3a6f811c5ca2f4e6aca70ada6ab5fb3e690
Ezzz-Lui <ogn.lui@gmail.com>
2025-07-25 17:42:33 -0600

    Add LoginPage component for user authentication

---
+++ src/modules/auth/views/LoginPage.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+    <h1 class="text-2xl font-semibold mb-4">Login</h1>
+    <form>
+      <!-- Username Input -->
+      <div class="mb-4">
+        <label for="username" class="block text-gray-600">Username</label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-stone-500"
+          autocomplete="off"
+        />
+      </div>
+      <!-- Password Input -->
+      <div class="mb-4">
+        <label for="password" class="block text-gray-600">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-stone-500"
+          autocomplete="off"
+        />
+      </div>
+      <!-- Remember Me Checkbox -->
+      <div class="mb-4 flex items-center">
+        <input type="checkbox" id="remember" name="remember" class="text-stone-500" />
+        <label for="remember" class="text-gray-600 ml-2">Remember Me</label>
+      </div>
+      <!-- Forgot Password Link -->
+      <div class="mb-6 text-stone-500">
+        <span class="hover:underline">Forgot Password?</span>
+      </div>
+      <!-- Login Button -->
+      <button
+        @click="onLogin"
+        type="button"
+        class="bg-stone-900 hover:bg-stone-700 text-white font-semibold rounded-md py-2 px-4 w-full"
+      >
+        Login
+      </button>
+    </form>
+    <!-- Sign up  Link -->
+    <div class="mt-6 text-stone-500 text-center font-light">
+      <RouterLink :to="{ name: 'Register' }" class="hover:underline">Sign up Here</RouterLink>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const onLogin = () => {
+  localStorage.setItem('userId', 'ID-ABC');
+  const lastPath = localStorage.getItem('lastPath') ?? '/';
+
+  router.replace(lastPath);
+};
+</script>
