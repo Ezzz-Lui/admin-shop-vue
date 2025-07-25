cd6f6303a905d8b220b670441acd86351835b97c
Ezzz-Lui <ogn.lui@gmail.com>
2025-07-25 17:45:18 -0600

    Remove placeholder image from AuthLayout and set background color for left section

--- src/modules/auth/layouts/AuthLayout.vue
+++ src/modules/auth/layouts/AuthLayout.vue
@@ -1,13 +1,7 @@
 <template>
   <div class="flex justify-center items-center h-screen">
     <!-- Left: Image -->
-    <div class="w-1/2 h-screen hidden lg:block">
-      <img
-        src="https://placehold.co/800x/0c0a09/ffffff.png?text=Your+Image&font=Poppins"
-        alt="Placeholder Image"
-        class="object-cover w-full h-full"
-      />
-    </div>
+    <div class="w-1/2 h-screen hidden lg:block bg-stone-950"></div>
     <!-- Right: Form -->
     <RouterView />
   </div>
