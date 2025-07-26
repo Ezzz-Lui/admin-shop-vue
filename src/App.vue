ae4e91ac4bcb31e931ed800d926739d34d360772
Ezzz-Lui <ogn.lui@gmail.com>
2025-07-25 19:46:34 -0600

    Add Toaster component to App.vue for enhanced notification support

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
+  <Toaster rich-colors position="top-right" />
   <RouterView />
   <VueQueryDevtools />
 </template>
 
 <script setup lang="ts">
 import { VueQueryDevtools } from '@tanstack/vue-query-devtools';
+import { Toaster } from '@/components/ui/sonner';
+import 'vue-sonner/style.css'; //
 </script>
