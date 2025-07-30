263b435cc2ebf5ba92770685c35ac766c1697c69
Ezzz-Lui <ogn.lui@gmail.com>
2025-07-30 02:33:30 -0600

    Refactor RegisterPage.vue to update label from 'Username' to 'Name' and remove 'Remember Me' checkbox for improved clarity

--- src/modules/auth/views/RegisterPage.vue
+++ src/modules/auth/views/RegisterPage.vue
@@ -4,11 +4,23 @@
     <form action="#" method="POST">
       <!-- Username Input -->
       <div class="mb-4">
-        <label for="username" class="block text-stone-600">Username</label>
+        <label for="username" class="block text-stone-600">Name</label>
         <input
-          type="text"
+          type="username"
           id="username"
           name="username"
+          class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+          autocomplete="off"
+        />
+      </div>
+
+      <!-- Username Input -->
+      <div class="mb-4">
+        <label for="email" class="block text-stone-600">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
           class="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
           autocomplete="off"
         />
@@ -24,11 +36,7 @@
           autocomplete="off"
         />
       </div>
-      <!-- Remember Me Checkbox -->
-      <div class="mb-4 flex items-center">
-        <input type="checkbox" id="remember" name="remember" class="text-stone-500" />
-        <label for="remember" class="text-stone-600 ml-2">Remember Me</label>
-      </div>
+
       <!-- Forgot Password Link -->
       <div class="mb-6 text-stone-500">
         <a href="#" class="hover:underline">Forgot Password?</a>
