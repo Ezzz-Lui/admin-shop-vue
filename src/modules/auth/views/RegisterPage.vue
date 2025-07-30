df122e0ad8f23be43086660c85268da83e942821
Ezzz-Lui <ogn.lui@gmail.com>
2025-07-30 03:31:55 -0600

    Refactor RegisterPage.vue to implement form validation, bind input fields to reactive state, and enhance loading state feedback during registration

--- src/modules/auth/views/RegisterPage.vue
+++ src/modules/auth/views/RegisterPage.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
     <h1 class="text-4xl font-semibold mb-4">Register</h1>
-    <form action="#" method="POST">
+    <form @submit.prevent="onRegister">
       <!-- Username Input -->
       <div class="mb-4">
         <label for="username" class="block text-stone-600">Name</label>
         <input
+          v-model="registerForm.fullName"
           type="username"
           id="username"
           name="username"
@@ -14,10 +15,11 @@
         />
       </div>
 
-      <!-- Username Input -->
+      <!-- Email Input -->
       <div class="mb-4">
         <label for="email" class="block text-stone-600">Email</label>
         <input
+          v-model="registerForm.email"
           type="email"
           id="email"
           name="email"
@@ -29,6 +31,7 @@
       <div class="mb-4">
         <label for="password" class="block text-stone-600">Password</label>
         <input
+          v-model="registerForm.password"
           type="password"
           id="password"
           name="password"
@@ -43,11 +46,15 @@
       </div>
       <!-- Register Button -->
       <button
-        @click="onRegister"
-        type="button"
+        type="submit"
+        :disabled="!registerForm.password || !registerForm.email || !registerForm.fullName"
         class="bg-zinc-900 hover:bg-zinc-800 text-white font-semibold rounded-md py-2 px-4 w-full"
       >
-        Register
+        <template v-if="isLoading">
+          <svg class="mr-3 size-5 animate-spin" viewBox="0 0 24 24"></svg>
+        </template>
+
+        <template v-else> Register </template>
       </button>
     </form>
     <!-- Sign up  Link -->
@@ -60,13 +67,41 @@
 </template>
 
 <script setup lang="ts">
+import { reactive, ref } from 'vue';
+import { useAuthStore } from '../store/auth.store';
+import { toast } from 'vue-sonner';
 import { useRouter } from 'vue-router';
 
-const router = useRouter();
+const registerForm = reactive({
+  fullName: '',
+  email: '',
+  password: '',
+});
 
-const onRegister = () => {
-  router.replace({
-    name: 'home',
-  });
+const isLoading = ref<boolean>(false);
+const router = useRouter();
+const { registerUser } = useAuthStore();
+
+const onRegister = async () => {
+  isLoading.value = true;
+  const ok = await registerUser(registerForm.fullName, registerForm.email, registerForm.password);
+
+  if (ok) {
+    toast.success('User registered success', {
+      description: 'Redirecting to shop...',
+    });
+
+    isLoading.value = false;
+    router.push({ name: 'home' });
+  } else {
+    toast.error('Fail user registration', {
+      description: 'Try again',
+      action: {
+        label: 'Ok',
+      },
+    });
+
+    isLoading.value = false;
+  }
 };
 </script>
