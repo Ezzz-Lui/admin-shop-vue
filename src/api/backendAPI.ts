5e0d09cb63d1cf47b702f3ad35fa2f45ec735301
Ezzz-Lui <ogn.lui@gmail.com>
2025-07-30 03:53:04 -0600

    Refactor backendAPI.ts for consistent formatting and improved readability

--- src/api/backendAPI.ts
+++ src/api/backendAPI.ts
@@ -1,12 +1,19 @@
-import axios from "axios";
+import axios from 'axios';
 
 const backendAPI = axios.create({
   baseURL: import.meta.env.VITE_BACKEND_API_URL,
-})
-console.log(import.meta.env)
+});
+console.log(import.meta.env);
 
 // Interceptors
+backendAPI.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
 
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
+  return config;
+});
 
 export { backendAPI };
