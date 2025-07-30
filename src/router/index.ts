0b14cd0fe402fef3b113e1346d897bd5cb54bb76
Ezzz-Lui <ogn.lui@gmail.com>
2025-07-30 06:38:42 -0600

    Add admin route with guards for access control

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,6 @@
+import AdminLayout from '@/modules/admin/layouts/AdminLayout.vue';
+import isAuthenticated from '@/modules/auth/guards/is-authenticated.guard';
+import isAdminGuard from '@/modules/auth/guards/isAdmin.guard';
 import { authRoutes } from '@/modules/auth/routes';
 import ShopLayout from '@/modules/shop/layouts/ShopLayout.vue';
 import { createRouter, createWebHistory } from 'vue-router';
@@ -14,6 +17,20 @@ const router = createRouter({
           path: '',
           name: 'home',
           component: () => import('@/modules/shop/views/HomeShop.vue'),
+        },
+      ],
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      redirect: { name: 'AdminPanel' },
+      component: AdminLayout,
+      beforeEnter: [isAdminGuard, isAuthenticated],
+      children: [
+        {
+          path: '',
+          name: 'AdminPanel',
+          component: () => import('@/modules/admin/views/AdminHome.vue'),
         },
       ],
     },
