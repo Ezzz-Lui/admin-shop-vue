9caf11025e71a6aa75fab089233873f8088f39a9
Ezzz-Lui <ogn.lui@gmail.com>
2025-11-12 20:54:18 -0600

    Add login background image to AuthLayout component for improved visual appeal

--- src/modules/auth/layouts/AuthLayout.vue
+++ src/modules/auth/layouts/AuthLayout.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex justify-center items-center h-screen">
     <!-- Left: Image -->
-    <div class="w-1/2 h-screen hidden lg:block bg-stone-900"></div>
+    <img src="/src/assets/images/login.png" alt="Auth background" class="w-1/2 h-screen hidden lg:block object-cover">
     <!-- Right: Form -->
     <RouterView />
   </div>
